d95a0e3495557fbb9517ef42989017c3519e343e
P-Vries <43247243+P-Vries@users.noreply.github.com>
2019-05-17 11:18:26 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//using GameOfLifeTwo;
+
+namespace GameOfLifeTwoWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int defaultTickSpeed = 1000;
+        public int currentTickSpeed = 1000;
+        public make grid = new make();
+        Timer tick = new Timer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void render()
+        {
+            const int heightGrid = make.gridLength;
+            const int widthGrid = make.gridLength;
+
+            for (int x = 0; x < heightGrid; x++)
+            {
+                for (int y = 0; y < widthGrid; y++)
+                {
+                    Rectangle cell = new Rectangle();
+                    cell.Width = golCanvas.ActualWidth / widthGrid -1;
+                    cell.Height = golCanvas.ActualHeight / heightGrid -1;
+                    cell.Tag = x.ToString() + "," + y.ToString();
+                    if (grid.grid[x,y] == 1)
+                    {
+                        cell.Fill = Brushes.Yellow;
+                    }
+                    else
+                    {
+                        cell.Fill = Brushes.LightGray;
+                    }
+                    cell.MouseLeftButtonDown += Cell_MouseLeftButtonDown;
+                    golCanvas.Children.Add(cell);
+                    Canvas.SetLeft(cell, y * golCanvas.ActualWidth / widthGrid);
+                    Canvas.SetTop(cell, x * golCanvas.ActualHeight / heightGrid);
+                }
+            }
+            grid.grid = (int[,])grid.tempgrid.Clone();
+        }
+
+
+        private void Cell_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            int temX = int.Parse((((Rectangle)sender).Tag.ToString()).Split(',')[0]);
+            int temY = int.Parse((((Rectangle)sender).Tag.ToString()).Split(',')[1]);
+            if (grid.grid[temX, temY] == 0)
+            {
+                grid.tempgrid[temX, temY] = 1;
+            }
+            else grid.tempgrid[temX, temY] = 0;
+            grid.grid = (int[,])grid.tempgrid.Clone();
+            for (int i = 0; i < golCanvas.Children.Count; i++)
+            {
+                int tempX = int.Parse((((Rectangle)golCanvas.Children[i]).Tag.ToString()).Split(',')[0]);
+                int tempY = int.Parse((((Rectangle)golCanvas.Children[i]).Tag.ToString()).Split(',')[1]);
+
+                if (tempX == temX && tempY == temY)
+                {
+                    if(grid.grid[tempX, tempY] == 1) ((Rectangle)golCanvas.Children[i]).Fill = Brushes.Yellow;
+                    else ((Rectangle)golCanvas.Children[i]).Fill = Brushes.LightGray;
+                }
+            }
+
+
+        }
+
+        public void update()
+        {
+            
+            for (int i = 0; i < golCanvas.Children.Count; i++)
+            {
+                int tempX = int.Parse((((Rectangle)golCanvas.Children[i]).Tag.ToString()).Split(',')[0]);
+                int tempY = int.Parse((((Rectangle)golCanvas.Children[i]).Tag.ToString()).Split(',')[1]);
+
+                if (grid.grid[tempX,tempY] == 0)
+                {
+                    ((Rectangle)golCanvas.Children[i]).Fill = Brushes.LightGray;
+                }
+                else
+                {
+                    ((Rectangle)golCanvas.Children[i]).Fill = Brushes.Yellow;
+                }
+
+            }
+            grid.grid = (int[,])grid.tempgrid.Clone();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {       
+            grid.makeGrid();
+            render();
+            tick.Tick += Tick_Elapsed;
+            tick.Interval = currentTickSpeed;
+            tick.Enabled = false;
+        }
+
+        private void Tick_Elapsed(object sender, EventArgs e)
+        {
+            tick.Enabled = false;
+            grid.checkPos();
+            update();
+            tick.Start();
+            tick.Enabled = true;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            grid.checkPos();
+            grid.grid = (int[,])grid.tempgrid.Clone();
+            update();
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            if ((string)btnStart.Content == "Start")
+            {
+                tick.Enabled = true;
+                btnStart.Content = "Stop";
+            }
+            else
+            {
+                tick.Enabled = false;
+                btnStart.Content = "Start";
+            }
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            currentTickSpeed = defaultTickSpeed - (int)sldrSpeed.Value * 100 + 1;
+            tick.Interval = currentTickSpeed;
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            for (int x = 0; x < grid.grid.GetLength(0); x++)
+            {
+                for (int y = 0; y < grid.grid.GetLength(1); y++)
+                {
+                    grid.tempgrid[x, y] = 0;
+
+                }
+            }
+            grid.grid = (int[,])grid.tempgrid.Clone();
+            update();
+
+        }
+    }
+}
---
+++ make.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace GameOfLifeTwoWPF
+{
+    public class make
+    {
+        public const int gridLength = 25;
+        public int[,] grid = new int[gridLength, gridLength];
+        public int[,] tempgrid = new int[gridLength, gridLength];
+
+        Random alive = new Random();
+
+
+        public void makeGrid()
+        {
+            for (int x = 1; x < gridLength - 1; x++)
+            {
+                for (int y = 1; y < gridLength - 1; y++)
+                {
+                    int rand = alive.Next(0, 5);
+                    rand = (rand < 1 ? 1 : 0);
+                    grid[x, y] = rand;
+                }
+            }/*
+            grid = new int[10, 10] { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                     { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                     { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                     { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                     { 0, 0, 0, 1, 1, 1, 0, 0, 0, 0 },
+                                     { 0, 0, 1, 0, 0, 0, 1, 0, 0, 0 },
+                                     { 0, 0, 0, 1, 1, 1, 0, 0, 0, 0 },
+                                     { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                     { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                     { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }};
+                                     */
+            tempgrid = (int[,])grid.Clone();
+
+        }
+
+        public void checkPos()
+        {
+            for (int x = 0; x < gridLength - 1; x++)
+            {
+                for (int y = 0; y < gridLength - 1; y++)
+                {
+                    action(checkAround(x, y), x, y);
+                }
+            }
+        }
+
+        public int checkAround(int x, int y)
+        {
+            int check = 0;
+            List<int> dir = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7 };
+            List<int> exception = new List<int>();
+
+
+            switch (x)
+            {
+                case 0:
+                    exception.Add(0);
+                    exception.Add(1);
+                    exception.Add(2);
+                    break;
+                case gridLength:
+                    exception.Add(5);
+                    exception.Add(6);
+                    exception.Add(7);
+                    break;
+            }
+
+            switch (y)
+            {
+                case 0:
+                    exception.Add(0);
+                    exception.Add(3);
+                    exception.Add(5);
+                    break;
+                case gridLength:
+                    exception.Add(2);
+                    exception.Add(4);
+                    exception.Add(7);
+                    break;
+            }
+
+            exception = exception.Distinct().ToList();
+            exception.Sort();
+            exception.Reverse();
+
+
+            for (int i = 0; i < exception.Count; i++)
+            {
+                dir.RemoveAt(exception[i]);
+            }
+
+            foreach (int i in dir)
+            {
+                int tempX = x;
+                int tempY = y;
+                check = check + checkCurrentPos(i, tempX, tempY);
+            }
+            return check;
+        }
+
+        public int checkCurrentPos(int i, int tempX, int tempY)
+        {
+            int check = 0;
+            switch (i)
+            {
+                case 0:
+                    tempX--;
+                    tempY--;
+                    if (grid[tempX, tempY] == 1)
+                    {
+                        check++;
+                    }
+                    break;
+                case 1:
+                    tempX--;
+                    if (grid[tempX, tempY] == 1)
+                    {
+                        check++;
+                    }
+                    break;
+                case 2:
+                    tempX--;
+                    tempY++;
+                    if (grid[tempX, tempY] == 1)
+                    {
+                        check++;
+                    }
+                    break;
+                case 3:
+                    tempY--;
+                    if (grid[tempX, tempY] == 1)
+                    {
+                        check++;
+                    }
+                    break;
+                case 4:
+                    tempY++;
+                    if (grid[tempX, tempY] == 1)
+                    {
+                        check++;
+                    }
+                    break;
+                case 5:
+                    tempX++;
+                    tempY--;
+                    if (grid[tempX, tempY] == 1)
+                    {
+                        check++;
+                    }
+                    break;
+                case 6:
+                    tempX++;
+                    if (grid[tempX, tempY] == 1)
+                    {
+                        check++;
+                    }
+                    break;
+                case 7:
+                    tempX++;
+                    tempY++;
+                    if (grid[tempX, tempY] == 1)
+                    {
+                        check++;
+                    }
+                    break;
+            }
+            return check;
+        }
+
+        public void action(int check, int x, int y)
+        {
+
+            if (grid[x, y] == 1)
+            {
+                if (check == 0 || check == 1 || check >= 4)
+                {
+                    tempgrid[x, y] = 0;
+                }
+            }
+            else
+            {
+                if (check == 3)
+                {
+                    tempgrid[x, y] = 1;
+                }
+            }
+        }
+    }
+}
